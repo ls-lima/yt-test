e19ea360163f80187fa605892ef644d8146d1669
Lucas Lima <ls.lima@outlook.com.br>
2017-11-23 13:37:01 -0200

    Melhoras no layout e animações parciais na Home

--- app/app.js
+++ app/app.js
@@ -1,19 +1,29 @@
 var app = angular.module('app',['ngRoute', 'ngMaterial']);
 
-app.config(function($routeProvider, $locationProvider)
+app.config(function($routeProvider, $locationProvider, $mdThemingProvider)
 {
-   $locationProvider.html5Mode(true);
+  $mdThemingProvider.alwaysWatchTheme(true);
+  
+  $mdThemingProvider
+  .theme('default')
+  .primaryPalette('blue')
+  .accentPalette('teal')
+  .warnPalette('red')
+  .backgroundPalette('grey');
 
-   $routeProvider
+
+  $locationProvider.html5Mode(true);
+
+  $routeProvider
 
    // para a rota '/', carregaremos o template home.html e o controller 'HomeCtrl'
-   .when('/', {
-      templateUrl : 'app/views/home.html',
-      controller     : 'HomeCtrl',
-   })
-   .when('/sobre', {
-      templateUrl : 'app/views/details.html',
-      controller  : 'DetailsCtrl',
-   })
-   .otherwise ({ redirectTo: '/' });
+  .when('/', {
+    templateUrl : 'app/views/home.html',
+    controller     : 'HomeCtrl',
+  })
+  .when('/sobre', {
+    templateUrl : 'app/views/details.html',
+    controller  : 'DetailsCtrl',
+  })
+  .otherwise ({ redirectTo: '/' });
 });
